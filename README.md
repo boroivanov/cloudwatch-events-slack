66055e2513f8f0a9ef9b7cd397bf818d788c1a52
Borislav Ivanov <borogl@gmail.com>
2019-02-06 17:08:01 -0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Handle CloudWatch Events and post to Slack
+
+
+# Install / Update
+1. Create a Slack application `AWS Notifications`.
+2. Get the slack token from `AWS Notifications`.
+3. Create/Update the `.env.yml` file with the token.
+```bash
+$ cat .env.yml
+lambda:
+  environment:
+    SLACK_API_TOKEN: "xoxa-11111111111-1111111111111-1111111111111-abcd3abcd3abcd3abcd3abcd3abcd3123"
+    SLACK_CHANNEL: "aws-notifications"
+```
+4. Setup python venv and install modules for local development.
+```bash
+virtualenv -p `which python3` venv
+source venv/bin/activate
+pip install -U -r requirements.txt
+```
+5. Install/Update the infrastructure.
+```bash
+npm install serverless -g
+
+sls deploy <--aws-profile my-rt-profile>  --stage prod
+```
---
+++ codepipeline.py
@@ -0,0 +1,65 @@
+import os
+import json
+
+from slackclient import SlackClient
+
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+channel = os.environ['SLACK_CHANNEL']
+
+
+def lambda_handler(event, context):
+    # For debugging so you can see raw event format.
+    print('Here is the event:')
+    print(json.dumps(event))
+
+    if event['source'] != 'aws.codepipeline':
+        raise ValueError('Function only supports input from events with'
+                         'a source type of: aws.codepipeline')
+
+    sc = SlackClient(slack_token)
+
+    details = event['detail']
+
+    # Don't notify on the following
+    if details['stage'] == 'Source':
+        print('Skipping Slack notification for {}'.format(details['stage']))
+        return
+    if details['stage'] == 'Build' and details['state'] == 'SUCCEEDED':
+        print('Skipping Slack notification for {} {}'.format(
+            details['stage'], details['state']))
+        return
+
+    # Map Slack colors to states
+    if details['state'] in ['FAILED', 'CANCELED']:
+        color = 'danger'
+    elif details['state'] in ['SUCCEEDED', 'RESUMED']:
+        color = 'good'
+    else:
+        color = 'warning'
+
+    params = {
+        'channel': channel,
+        'attachments': [
+            {
+                'color': color,
+                'text': '{} {} {}'.format(details['pipeline'],
+                                          details['stage'],
+                                          details['state'])
+            }
+        ]
+    }
+    response = sc.api_call('chat.postMessage', **params)
+    print(response)
+
+
+if __name__ == '__main__':
+    event = {
+        'source': 'aws.codepipeline',
+        'detail': {
+            'pipeline': 'test-pipeline',
+            'stage': 'stage',
+            'state': 'FAILED'
+        }
+    }
+    lambda_handler(event, None)
